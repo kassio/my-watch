91ba870104f021731d7a29ea96777040ff0f7cc1
Kassio Borges <kassioborgesm@gmail.com>
2019-04-21 21:49:36 -0500

    Basic watch with date and battery stats

--- app/index.js
+++ app/index.js
@@ -1,19 +1,49 @@
-import clock from "clock";
-import document from "document";
-import { watchTime, watchDate } from "./components.js";
+import clock from 'clock';
+import document from 'document';
+import { watchTime, watchDate } from './components.js';
 
-import { battery, charger } from "power";
-console.log("The charger " + (charger.connected ? "is" : "is not") + " connected");
-console.log(battery.chargeLevel + "%");
+import { battery, charger } from 'power';
 
-clock.granularity = "minutes";
+clock.granularity = 'minutes';
 
-clock.ontick = (evt) => {
+clock.ontick = evt => {
+  setTime(evt);
+  setDate(evt);
+};
+
+battery.onchange = () => {
+  updateBatteryIcon();
+};
+
+const setTime = evt => {
   const { hours, minutes } = watchTime(evt);
-  const { weekDay, day, month } = watchDate(evt);
-  const watchTimeLabel = document.getElementById("time");
-  const watchDateLabel = document.getElementById("date");
-  
+  const watchTimeLabel = document.getElementById('time');
+
   watchTimeLabel.text = `${hours}:${minutes}`;
+};
+
+const setDate = evt => {
+  const { weekDay, day, month } = watchDate(evt);
+  const watchDateLabel = document.getElementById('date');
+
   watchDateLabel.text = `${weekDay}, ${day}/${month}`;
-}
+};
+
+const updateBatteryIcon = () => {
+  const icon = document.getElementById('battery-icon');
+  const batteryLabel = document.getElementById('battery');
+  const batteryLevel = Math.floor(battery.chargeLevel);
+  const goodPower = charger.connected && !charger.powerIsGood ? '!' : '';
+
+  batteryLabel.text = `${goodPower}${batteryLevel}%`;
+
+  if (batteryLevel < 17 || charger.connected) {
+    icon.width = 0;
+  } else if (batteryLevel < 50) {
+    icon.style.fill = 'orange';
+    icon.width = batteryLevel * 5 / 10;
+  } else {
+    icon.style.fill = 'green';
+    icon.width = batteryLevel * 5 / 10;
+  }
+};
